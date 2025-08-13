23569ceaf7beb31ddfe75b3d6334984949a05a38
XianyB <70772484+XianyB@users.noreply.github.com>
2025-08-13 20:33:23 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html'
+        // lägg till dina JS- och CSS-filer här om du vill cacha dem
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
